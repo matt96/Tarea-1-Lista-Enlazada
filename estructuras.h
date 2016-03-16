ddde07fffc08cad8d927775e4496ebb8cf00d3d4
c-bas97 <sebastian2015055146@gmail.com>
2016-03-16 12:18:36 -0600

    Estructuras

---
+++ estructuras.h
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct alien_t {
+	char nombre [30];
+	int id;
+	char * especie;
+	struct planetas_t * planetas;
+	struct idiomas_t * idiomas;
+	struct alien_t * siguiente;
+} typedef alien;
+
+struct planetas_t {
+	char nombre[30];
+	int starwars_code;
+	char agua;
+	char oxigeno;
+	struct planetas_t * siguiente;
+} typedef planeta;
+
+struct idiomas_t {
+	char nombre[30];
+	char tipo;
+	int simbolos;
+	struct idiomas_t * siguiente;
+	struct planetas_t * planetas;
+} typedef idioma;
